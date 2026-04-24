e483af564d01d07749706e868960ded2d4ae17fa
sio2000 <xsiwzos@gmail.com>
2026-04-24 16:14:22 +0300

    Updated tailwind.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
-import { useScrollReveal } from './hooks/useScrollReveal';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import MarqueeBanner from './components/MarqueeBanner';
-import WhyItWorks from './components/WhyItWorks';
-import Programs from './components/Programs';
-import SuccessStories from './components/SuccessStories';
-import Experience from './components/Experience';
-import FinalCTA from './components/FinalCTA';
-import Footer from './components/Footer';
-
 function App() {
-  useScrollReveal();
-
   return (
-    <div className="min-h-screen bg-[#080808]">
-      <Navbar />
-      <Hero />
-      <MarqueeBanner />
-      <WhyItWorks />
-      <div className="section-divider" />
-      <Programs />
-      <div className="section-divider" />
-      <SuccessStories />
-      <div className="section-divider" />
-      <Experience />
-      <div className="section-divider" />
-      <FinalCTA />
-      <Footer />
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <p>Start prompting (or editing) to see magic happen :)</p>
     </div>
   );
 }
